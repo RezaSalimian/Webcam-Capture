5a513298b546bfdbf1bb1757495fd907ca8f9fd5
Mreza <mrsalimian66@gmail.com>
2022-03-15 13:11:06 -0400

    first step

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-import kivy
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager,screen
+from kivy.lang import Builder
+Builder.load_file('frontend.kv')
+
+class RootWidge(ScreenManager):
+    pass
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+MainApp().run()
