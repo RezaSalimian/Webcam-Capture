e0ea0865b19d22edc9446b55ca9d170163a30622
Mreza <mrsalimian66@gmail.com>
2022-03-15 16:55:32 -0400

    first step

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.lang import Builder
+import time
 #from fileShare import FileSharer
 Builder.load_file('frontend.kv')
 
 class CameraScreen(Screen):
     def start(self):
         self.ids.camera.play = True
+        self.ids.camera_button.text ='Stop Camera'
+        self.ids.camera.texture = self.ids.camera._camera.texture
     def stop(self):
-        pass
+        self.ids.camera.play = False
+        self.ids.camera_button.text = 'Start Camera'
+        self.ids.camera.texture =None
     def capture(self):
-        pass
+        filename=time.strftime('%Y%m%d-%H%M%S')+"png"
+        self.ids.camera.export_to_png(filename)
 class ImageScreen(Screen):
     pass
 
