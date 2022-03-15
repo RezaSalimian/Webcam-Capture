e31b4572e5817a166082460d3994c31bbdd408b3
Mreza <mrsalimian66@gmail.com>
2022-03-15 16:21:07 -0400

    first step

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager,screen
+from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.lang import Builder
+#from fileShare import FileSharer
 Builder.load_file('frontend.kv')
 
-class RootWidge(ScreenManager):
+class CameraScreen(Screen):
+    def start(self):
+        self.ids.camera.play = True
+    def stop(self):
+        pass
+    def capture(self):
+        pass
+class ImageScreen(Screen):
+    pass
+
+
+class RootWidget(ScreenManager):
     pass
 class MainApp(App):
     def build(self):
